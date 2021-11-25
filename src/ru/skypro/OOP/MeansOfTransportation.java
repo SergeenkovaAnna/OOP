2d3fa7936adb49a41c1d311be54bab0034f74c07
AnnaSergeenkova <lutekAnn@yandex.ru>
2021-11-25 23:48:48 +0300

    на проверку

---
+++ src/ru/skypro/OOP/MeansOfTransportation.java
@@ -0,0 +1,31 @@
+package ru.skypro.OOP;
+
+public class MeansOfTransportation {
+    public String modelName;
+    public int wheelsCount;
+
+    public MeansOfTransportation(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
